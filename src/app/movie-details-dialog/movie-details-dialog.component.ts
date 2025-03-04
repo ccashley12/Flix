4dab2baf4f7a58e00b69fd930443ff1ba6399e83
ccashley12 <ccashley12@gmail.com>
2025-03-04 11:57:50 -0500

    added move details component

---
+++ src/app/movie-details-dialog/movie-details-dialog.component.ts
@@ -0,0 +1,65 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { GenreDialogComponent } from '../genre-dialog/genre-dialog.component';
+import { DirectorDialogComponent } from '../director-dialog/director-dialog.component';
+
+@Component({
+  selector: 'app-movie-details-dialog',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    CommonModule,
+    MatIconModule,
+  ],
+  templateUrl: './movie-details-dialog.component.html',
+  styleUrls: ['./movie-details-dialog.component.scss'],
+})
+export class MovieDetailsDialogComponent implements OnInit {
+  movie: any;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any, // Data passed to dialog
+    private fetchApiData: FetchApiDataService, // Service to fetch data from API
+    private dialog: MatDialog, // Service to open dialog windows
+    private dialogRef: MatDialogRef<MovieDetailsDialogComponent>, // Reference to current dialog instance
+  ) {}
+
+  ngOnInit(): void {
+    // Initialize component and assign movie data
+    if (typeof this.data.movie.genre === 'string') {
+      this.data.movie.genre = { name: this.data.movie.genre, Description: '' };
+      this.movie = this.data.movie;
+    }
+  }
+
+  // Open genre dialog
+  openGenreDialog(genre: any): void {
+    this.fetchApiData.getGenre(genre.Name).subscribe((genreData) => {
+      console.log('Genre data fetched:', genreData);
+      this.dialog.open(GenreDialogComponent, {
+        data: { genre: genreData },
+        width: '600px',
+      });
+      this.dialogRef.close();
+    });
+  }
+  // Open director dialog
+  openDirectorDialog(director: string): void {
+    this.fetchApiData.getDirector(director).subscribe((director) => {
+      this.dialog.open(DirectorDialogComponent, {
+        data: { director },
+        width: '600px',
+      });
+      this.dialogRef.close();
+    });
+  }
+}
