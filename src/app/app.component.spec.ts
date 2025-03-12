72f9a66c61cc5a6f6713d075b84a988ae2c51639
ccashley12 <ccashley12@gmail.com>
2025-03-12 11:28:04 -0400

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ShadowKick' title`, () => {
+  it(`should have the 'CinemaExpress' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ShadowKick');
+    expect(app.title).toEqual('CinemaExpress');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, ShadowKick',
+      'Hello, CinemaExpress',
     );
   });
 });
