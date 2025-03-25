3518ea3d36bba6329af45198bd5223b13a054ce2
ccashley12 <ccashley12@gmail.com>
2025-03-25 18:15:38 -0400

    imported common module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
-import { GenreDialogComponent } from './genre-dialog/genre-dialog.component';
-import { DirectorDialogComponent } from './director-dialog/director-dialog.component';
-import { MovieDetailsDialogComponent } from './movie-details-dialog/movie-details-dialog.component';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, MatDialogModule],
-  providers: [
-    {
-      provide: MatDialogRef,
-      useValue: {}
-    },
-    GenreDialogComponent,
-    DirectorDialogComponent,
-    MovieDetailsDialogComponent
-  ],
+  imports: [CommonModule, RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
