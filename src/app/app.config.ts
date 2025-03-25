8ca2fec8961ce0ed39d69478cc52da38fd8634de
ccashley12 <ccashley12@gmail.com>
2025-03-25 18:15:23 -0400

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
-    provideAnimationsAsync(),
     FetchApiDataService,
   ],
 };
